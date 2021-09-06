f5f1ea90055860509dcfb81bbe88b80a8dcadb83
arjansunar <arjan.gahatrajsunar@gmail.com>
2021-09-06 09:33:35 +0545

    creating hero section

--- components/HeroSection/index.jsx
+++ components/HeroSection/index.jsx
@@ -1,6 +1,6 @@
 import Button from "../Button/Button";
 import styles from "./Hero.module.scss";
-// import Img from "next/image";
+import Img from "next/image";
 
 function Hero() {
   return (
@@ -18,7 +18,11 @@ function Hero() {
       </div>
       <div>
         <div className={styles.imageWrapper}>
-          {/* <Img src="" width="421" height="452" /> */}
+          <Img
+            src="https://images.unsplash.com/photo-1567804154474-380ce535360e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
+            width="421"
+            height="452"
+          />
         </div>
       </div>
     </section>
